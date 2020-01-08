3c3bb9962491f6494561d43ef25b2321638a0ed6
Daniel Haag <danni.haag91@gmail.com>
2020-01-07 23:09:47 -0300

    Mensagem acresentada

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -11,6 +11,7 @@ namespace projetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
             Console.Read();
         }
     }
